open-gitagent/gitagent

feat: Implement workflow generation command with YAML validation and LLM integration
- Added `workflow.schema.json` for defining the structure of SkillFlow workflows.
- Created `workflow.ts` to handle the `gitclaw workflow generate` command, including parsing flags and invoking the LLM.
- Introduced `schemas.ts` for loading and validating workflows against the defined schema.
- Developed `workflow-generator.ts` to manage LLM interactions and generate workflows based on user prompts.
- Implemented tests for workflow generation and validation to ensure functionality and correctness.
- Enhanced `package.json` test script for improved testing capabilities.

src/commands/workflow.ts
@@ -1,0 +1,188 @@
+import { mkdir, readFile, writeFile } from "fs/promises";
+import { join, resolve } from "path";
+import { discoverSkills } from "../skills.js";
+import { validateWorkflow } from "../utils/schemas.js";
+import { generateWorkflow, type LlmClient } from "../utils/workflow-generator.js";
+
+interface GenerateFlags {
+	dir: string;
+	prompt?: string;
+	refine?: string;
+	model?: string;
+	apiKey?: string;
+	dryRun: boolean;
+}
+
+const RED = (s: string) => `\x1b[31m${s}\x1b[0m`;
+const GREEN = (s: string) => `\x1b[32m${s}\x1b[0m`;
+const DIM = (s: string) => `\x1b[2m${s}\x1b[0m`;
+const BOLD = (s: string) => `\x1b[1m${s}\x1b[0m`;
+
+const MAX_RETRIES = 2;
+
+function printHelp(): void {
+	console.log(`${BOLD("gitclaw workflow")} — generate SkillFlow workflows from natural language
+
+Usage:
+  gitclaw workflow generate [options]
+
+Options:
+  -d, --dir <path>        Agent directory (default: current directory)
+  -p, --prompt <text>     Natural-language description of the workflow (required)
+      --refine <file>     Refine an existing workflow YAML by applying --prompt as an instruction
+  -m, --model <spec>      LLM model in provider:model form (default: openai:gpt-4o)
+      --api-key <key>     API key for the provider (falls back to OPENAI_API_KEY or <PROVIDER>_API_KEY)
+      --dry-run           Print the generated YAML to stdout instead of writing a file
+  -h, --help              Show this help message
+
+Examples:
+  gitclaw workflow generate -p "every morning summarize unread emails and post to Slack"
+  gitclaw workflow generate -p "add a human approval step before the Slack post" --refine workflows/morning-digest.yaml
+`);
+}
+
+function parseFlags(argv: string[]): GenerateFlags {
+	const flags: GenerateFlags = { dir: process.cwd(), dryRun: false };
+	for (let i = 0; i < argv.length; i++) {
+		const a = argv[i];
+		switch (a) {
+			case "-d":
+			case "--dir":
+				flags.dir = argv[++i];
+				break;
+			case "-p":
+			case "--prompt":
+				flags.prompt = argv[++i];
+				break;
+			case "--refine":
+				flags.refine = argv[++i];
+				break;
+			case "-m":
+			case "--model":
+				flags.model = argv[++i];
+				break;
+			case "--api-key":
+				flags.apiKey = argv[++i];
+				break;
+			case "--dry-run":
+				flags.dryRun = true;
+				break;
+			case "-h":
+			case "--help":
+				printHelp();
+				process.exit(0);
+				break;
+			default:
+				if (!a.startsWith("-") && flags.prompt === undefined) {
+					flags.prompt = a;
+				} else {
+					console.error(RED(`Unknown option: ${a}`));
+					process.exit(2);
+				}
+		}
+	}
+	return flags;
+}
+
+function slugify(name: string): string {
+	const cleaned = name
+		.toLowerCase()
+		.trim()
+		.replace(/[^a-z0-9-]+/g, "-")
+		.replace(/^-+|-+$/g, "")
+		.replace(/-+/g, "-");
+	return cleaned || "workflow";
+}
+
+export interface RunGenerateOptions {
+	flags: GenerateFlags;
+	llm?: LlmClient;
+}
+
+export async function runGenerate(opts: RunGenerateOptions): Promise<{ filePath?: string; yaml: string; }> {
+	const { flags } = opts;
+	if (!flags.prompt || !flags.prompt.trim()) {
+		throw new Error("--prompt is required");
+	}
+
+	const agentDir = resolve(flags.dir);
+	const skills = await discoverSkills(agentDir);
+
+	let previousWorkflow: string | undefined;
+	if (flags.refine) {
+		const refinePath = resolve(agentDir, flags.refine);
+		previousWorkflow = await readFile(refinePath, "utf-8");
+	}
+
+	let promptForLlm = flags.prompt.trim();
+	let lastErrors: string[] = [];
+	let yaml = "";
+
+	for (let attempt = 0; attempt <= MAX_RETRIES; attempt++) {
+		console.error(DIM(attempt === 0 ? "Generating workflow..." : `Retry ${attempt}/${MAX_RETRIES} — fixing validation errors...`));
+		yaml = await generateWorkflow({
+			prompt: promptForLlm,
+			skills,
+			previousWorkflow,
+			model: flags.model,
+			apiKey: flags.apiKey,
+			llm: opts.llm,
+		});
+		const result = validateWorkflow(yaml);
+		if (result.valid) {
+			lastErrors = [];
+			break;
+		}
+		lastErrors = result.errors;
+		if (attempt < MAX_RETRIES) {
+			promptForLlm =
+				`${flags.prompt.trim()}\n\nThe previous attempt failed schema validation. Fix these errors and return the full YAML again:\n` +
+				result.errors.map((e) => `- ${e}`).join("\n");
+		}
+	}
+
+	if (lastErrors.length > 0) {
+		console.error(RED("\nWorkflow validation failed after retries:"));
+		for (const e of lastErrors) console.error(RED(`  - ${e}`));
+		console.error(DIM("\nLast generated YAML:\n"));
+		console.error(yaml);
+		throw new Error("Validation failed after retries");
+	}
+
+	if (flags.dryRun) {
+		process.stdout.write(yaml.endsWith("\n") ? yaml : yaml + "\n");
+		return { yaml };
+	}
+
+	// Parse the validated YAML to get the workflow name for the file path.
+	const validated = validateWorkflow(yaml).data!;
+	const slug = slugify(validated.name);
+	const workflowsDir = join(agentDir, "workflows");
+	await mkdir(workflowsDir, { recursive: true });
+	const filePath = join(workflowsDir, `${slug}.yaml`);
+	await writeFile(filePath, yaml.endsWith("\n") ? yaml : yaml + "\n", "utf-8");
+	console.error(GREEN(`\nWrote workflow to ${filePath}`));
+	return { filePath, yaml };
+}
+
+export async function handleWorkflowCommand(argv: string[]): Promise<void> {
+	// argv is the raw process.argv tail starting at the 'workflow' token.
+	// argv[0] === "workflow"; argv[1] is the sub-command.
+	const sub = argv[1];
+	if (!sub || sub === "-h" || sub === "--help") {
+		printHelp();
+		return;
+	}
+	if (sub !== "generate") {
+		console.error(RED(`Unknown subcommand: ${sub}`));
+		printHelp();
+		process.exit(2);
+	}
+	const flags = parseFlags(argv.slice(2));
+	try {
+		await runGenerate({ flags });
+	} catch (err: any) {
+		console.error(RED(`\nError: ${err?.message ?? String(err)}`));
+		process.exit(1);
+	}
+}

test/workflow-generator.test.ts
@@ -1,0 +1,222 @@
+// Unit tests for src/utils/workflow-generator.ts and the retry loop in
+// src/commands/workflow.ts. The LLM is fully mocked — no network calls.
+
+import test from "node:test";
+import assert from "node:assert/strict";
+import { mkdtemp, readFile, rm } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+
+import {
+	buildMessages,
+	buildSystemPrompt,
+	stripCodeFences,
+	generateWorkflow,
+	type LlmClient,
+	type LlmMessage,
+} from "../src/utils/workflow-generator.ts";
+import { runGenerate } from "../src/commands/workflow.ts";
+import type { SkillMetadata } from "../src/skills.ts";
+
+const SKILLS: SkillMetadata[] = [
+	{ name: "gmail", description: "Read and send email", directory: "/x/skills/gmail", filePath: "/x/skills/gmail/SKILL.md" },
+	{ name: "slack", description: "Post to Slack", directory: "/x/skills/slack", filePath: "/x/skills/slack/SKILL.md" },
+	{ name: "summarize", description: "Summarize text", directory: "/x/skills/summarize", filePath: "/x/skills/summarize/SKILL.md" },
+];
+
+const VALID_YAML = `name: morning-digest
+description: Summarize unread emails and post to Slack each morning.
+steps:
+  - skill: gmail
+    prompt: Fetch unread emails.
+  - skill: summarize
+    prompt: Compose a digest.
+  - skill: slack
+    prompt: Post the digest.
+    channel: "#daily-digest"
+`;
+
+const INVALID_YAML_MISSING_NAME = `description: no name here
+steps:
+  - skill: gmail
+    prompt: hi
+`;
+
+// ── buildSystemPrompt / buildMessages ──────────────────────────────────
+
+test("buildSystemPrompt embeds the schema text and the skill list", () => {
+	const sys = buildSystemPrompt(SKILLS);
+	assert.ok(sys.includes("<schema>"), "system prompt missing <schema> tag");
+	assert.ok(sys.includes('"$id": "https://gitclaw.dev/spec/workflow.schema.json"'), "system prompt missing schema $id");
+	assert.ok(sys.includes("- gmail: Read and send email"), "system prompt missing gmail skill");
+	assert.ok(sys.includes("- slack: Post to Slack"), "system prompt missing slack skill");
+	assert.ok(sys.includes("Output ONLY valid YAML"), "system prompt missing rule about raw YAML");
+});
+
+test("buildSystemPrompt handles empty skill list with a fallback hint", () => {
+	const sys = buildSystemPrompt([]);
+	assert.ok(sys.includes("no installed skills detected"), "system prompt missing empty-skills fallback");
+});
+
+test("buildMessages includes two few-shot pairs and the user prompt", () => {
+	const messages = buildMessages({ prompt: "Do the thing", skills: SKILLS });
+	assert.equal(messages[0].role, "system");
+	assert.equal(messages[1].role, "user");
+	assert.equal(messages[2].role, "assistant");
+	assert.equal(messages[3].role, "user");
+	assert.equal(messages[4].role, "assistant");
+	assert.equal(messages[5].role, "user");
+	assert.equal(messages[5].content, "Do the thing");
+});
+
+test("buildMessages wraps refine-mode prompts with the previous YAML and instruction", () => {
+	const messages = buildMessages({
+		prompt: "Add an approval step before the Slack post.",
+		skills: SKILLS,
+		previousWorkflow: VALID_YAML,
+	});
+	const last = messages[messages.length - 1];
+	assert.equal(last.role, "user");
+	assert.ok(last.content.includes("Here is the current workflow"));
+	assert.ok(last.content.includes("Add an approval step before the Slack post."));
+	assert.ok(last.content.includes("morning-digest"));
+	assert.ok(last.content.includes("Return the complete updated workflow as YAML — not a diff."));
+});
+
+// ── stripCodeFences ────────────────────────────────────────────────────
+
+test("stripCodeFences removes generic fenced code", () => {
+	const input = "```\nname: foo\n```\n";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+test("stripCodeFences removes yaml-tagged fences", () => {
+	const input = "```yaml\nname: foo\n```";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+test("stripCodeFences leaves unfenced YAML alone", () => {
+	const input = "name: foo\n";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+// ── generateWorkflow with an injected LLM ──────────────────────────────
+
+test("generateWorkflow returns the LLM output after stripping fences", async () => {
+	const captured: { messages: LlmMessage[] } = { messages: [] };
+	const llm: LlmClient = async (messages) => {
+		captured.messages = messages;
+		return "```yaml\n" + VALID_YAML + "```";
+	};
+	const out = await generateWorkflow({
+		prompt: "summarize emails and post to slack",
+		skills: SKILLS,
+		llm,
+	});
+	assert.equal(out.trim().startsWith("name: morning-digest"), true);
+	assert.equal(captured.messages.length, 6);
+	assert.equal(captured.messages[0].role, "system");
+});
+
+test("generateWorkflow throws if prompt is empty", async () => {
+	await assert.rejects(
+		() => generateWorkflow({ prompt: "   ", skills: SKILLS, llm: async () => VALID_YAML }),
+		/prompt is required/,
+	);
+});
+
+// ── Retry loop in runGenerate ──────────────────────────────────────────
+
+test("runGenerate retries when validation fails, then writes the file when the second attempt is valid", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitclaw-test-"));
+	try {
+		let calls = 0;
+		const llm: LlmClient = async (messages) => {
+			calls++;
+			const userMsg = messages[messages.length - 1].content;
+			if (calls === 1) return INVALID_YAML_MISSING_NAME;
+			// Second attempt: ensure the retry prompt included the validation error.
+			assert.ok(userMsg.includes("schema validation"), `retry user message did not mention validation: ${userMsg}`);
+			return VALID_YAML;
+		};
+		const result = await runGenerate({
+			flags: {
+				dir,
+				prompt: "summarize unread emails and post to Slack",
+				dryRun: false,
+			},
+			llm,
+		});
+		assert.equal(calls, 2);
+		assert.ok(result.filePath, "expected a written file path");
+		assert.equal(result.filePath!.endsWith("workflows/morning-digest.yaml"), true);
+		const written = await readFile(result.filePath!, "utf-8");
+		assert.ok(written.includes("name: morning-digest"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate honours --dry-run by returning YAML without writing", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitclaw-test-"));
+	try {
+		const llm: LlmClient = async () => VALID_YAML;
+		const result = await runGenerate({
+			flags: { dir, prompt: "x", dryRun: true },
+			llm,
+		});
+		assert.equal(result.filePath, undefined);
+		assert.ok(result.yaml.includes("name: morning-digest"));
+		// workflows/ must not have been created.
+		await assert.rejects(() => readFile(join(dir, "workflows", "morning-digest.yaml"), "utf-8"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate gives up after MAX_RETRIES and throws", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitclaw-test-"));
+	try {
+		let calls = 0;
+		const llm: LlmClient = async () => {
+			calls++;
+			return INVALID_YAML_MISSING_NAME;
+		};
+		await assert.rejects(
+			() => runGenerate({ flags: { dir, prompt: "x", dryRun: true }, llm }),
+			/Validation failed after retries/,
+		);
+		assert.equal(calls, 3); // 1 initial + 2 retries
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate refine mode reads previous YAML and passes it to the LLM", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitclaw-test-"));
+	try {
+		const { writeFile, mkdir } = await import("node:fs/promises");
+		await mkdir(join(dir, "workflows"), { recursive: true });
+		const refinePath = join(dir, "workflows", "starter.yaml");
+		await writeFile(refinePath, VALID_YAML, "utf-8");
+
+		let observed = "";
+		const llm: LlmClient = async (messages) => {
+			observed = messages[messages.length - 1].content;
+			return VALID_YAML;
+		};
+		await runGenerate({
+			flags: {
+				dir,
+				prompt: "add an approval step before slack",
+				refine: "workflows/starter.yaml",
+				dryRun: true,
+			},
+			llm,
+		});
+		assert.ok(observed.includes("Here is the current workflow"));
+		assert.ok(observed.includes("add an approval step before slack"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});

krishvsoni/gitclaw

src/commands/workflow.ts
@@ -1,0 +1,188 @@
+import { mkdir, readFile, writeFile } from "fs/promises";
+import { join, resolve } from "path";
+import { discoverSkills } from "../skills.js";
+import { validateWorkflow } from "../utils/schemas.js";
+import { generateWorkflow, type LlmClient } from "../utils/workflow-generator.js";
+
+interface GenerateFlags {
+	dir: string;
+	prompt?: string;
+	refine?: string;
+	model?: string;
+	apiKey?: string;
+	dryRun: boolean;
+}
+
+const RED = (s: string) => `\x1b[31m${s}\x1b[0m`;
+const GREEN = (s: string) => `\x1b[32m${s}\x1b[0m`;
+const DIM = (s: string) => `\x1b[2m${s}\x1b[0m`;
+const BOLD = (s: string) => `\x1b[1m${s}\x1b[0m`;
+
+const MAX_RETRIES = 2;
+
+function printHelp(): void {
+	console.log(`${BOLD("gitagent workflow")} — generate SkillFlow workflows from natural language
+
+Usage:
+  gitagent workflow generate [options]
+
+Options:
+  -d, --dir <path>        Agent directory (default: current directory)
+  -p, --prompt <text>     Natural-language description of the workflow (required)
+      --refine <file>     Refine an existing workflow YAML by applying --prompt as an instruction
+  -m, --model <spec>      LLM model in provider:model form (default: openai:gpt-4o)
+      --api-key <key>     API key for the provider (falls back to OPENAI_API_KEY or <PROVIDER>_API_KEY)
+      --dry-run           Print the generated YAML to stdout instead of writing a file
+  -h, --help              Show this help message
+
+Examples:
+  gitagent workflow generate -p "every morning summarize unread emails and post to Slack"
+  gitagent workflow generate -p "add a human approval step before the Slack post" --refine workflows/morning-digest.yaml
+`);
+}
+
+function parseFlags(argv: string[]): GenerateFlags {
+	const flags: GenerateFlags = { dir: process.cwd(), dryRun: false };
+	for (let i = 0; i < argv.length; i++) {
+		const a = argv[i];
+		switch (a) {
+			case "-d":
+			case "--dir":
+				flags.dir = argv[++i];
+				break;
+			case "-p":
+			case "--prompt":
+				flags.prompt = argv[++i];
+				break;
+			case "--refine":
+				flags.refine = argv[++i];
+				break;
+			case "-m":
+			case "--model":
+				flags.model = argv[++i];
+				break;
+			case "--api-key":
+				flags.apiKey = argv[++i];
+				break;
+			case "--dry-run":
+				flags.dryRun = true;
+				break;
+			case "-h":
+			case "--help":
+				printHelp();
+				process.exit(0);
+				break;
+			default:
+				if (!a.startsWith("-") && flags.prompt === undefined) {
+					flags.prompt = a;
+				} else {
+					console.error(RED(`Unknown option: ${a}`));
+					process.exit(2);
+				}
+		}
+	}
+	return flags;
+}
+
+function slugify(name: string): string {
+	const cleaned = name
+		.toLowerCase()
+		.trim()
+		.replace(/[^a-z0-9-]+/g, "-")
+		.replace(/^-+|-+$/g, "")
+		.replace(/-+/g, "-");
+	return cleaned || "workflow";
+}
+
+export interface RunGenerateOptions {
+	flags: GenerateFlags;
+	llm?: LlmClient;
+}
+
+export async function runGenerate(opts: RunGenerateOptions): Promise<{ filePath?: string; yaml: string; }> {
+	const { flags } = opts;
+	if (!flags.prompt || !flags.prompt.trim()) {
+		throw new Error("--prompt is required");
+	}
+
+	const agentDir = resolve(flags.dir);
+	const skills = await discoverSkills(agentDir);
+
+	let previousWorkflow: string | undefined;
+	if (flags.refine) {
+		const refinePath = resolve(agentDir, flags.refine);
+		previousWorkflow = await readFile(refinePath, "utf-8");
+	}
+
+	let promptForLlm = flags.prompt.trim();
+	let lastErrors: string[] = [];
+	let yaml = "";
+
+	for (let attempt = 0; attempt <= MAX_RETRIES; attempt++) {
+		console.error(DIM(attempt === 0 ? "Generating workflow..." : `Retry ${attempt}/${MAX_RETRIES} — fixing validation errors...`));
+		yaml = await generateWorkflow({
+			prompt: promptForLlm,
+			skills,
+			previousWorkflow,
+			model: flags.model,
+			apiKey: flags.apiKey,
+			llm: opts.llm,
+		});
+		const result = validateWorkflow(yaml);
+		if (result.valid) {
+			lastErrors = [];
+			break;
+		}
+		lastErrors = result.errors;
+		if (attempt < MAX_RETRIES) {
+			promptForLlm =
+				`${flags.prompt.trim()}\n\nThe previous attempt failed schema validation. Fix these errors and return the full YAML again:\n` +
+				result.errors.map((e) => `- ${e}`).join("\n");
+		}
+	}
+
+	if (lastErrors.length > 0) {
+		console.error(RED("\nWorkflow validation failed after retries:"));
+		for (const e of lastErrors) console.error(RED(`  - ${e}`));
+		console.error(DIM("\nLast generated YAML:\n"));
+		console.error(yaml);
+		throw new Error("Validation failed after retries");
+	}
+
+	if (flags.dryRun) {
+		process.stdout.write(yaml.endsWith("\n") ? yaml : yaml + "\n");
+		return { yaml };
+	}
+
+	// Parse the validated YAML to get the workflow name for the file path.
+	const validated = validateWorkflow(yaml).data!;
+	const slug = slugify(validated.name);
+	const workflowsDir = join(agentDir, "workflows");
+	await mkdir(workflowsDir, { recursive: true });
+	const filePath = join(workflowsDir, `${slug}.yaml`);
+	await writeFile(filePath, yaml.endsWith("\n") ? yaml : yaml + "\n", "utf-8");
+	console.error(GREEN(`\nWrote workflow to ${filePath}`));
+	return { filePath, yaml };
+}
+
+export async function handleWorkflowCommand(argv: string[]): Promise<void> {
+	// argv is the raw process.argv tail starting at the 'workflow' token.
+	// argv[0] === "workflow"; argv[1] is the sub-command.
+	const sub = argv[1];
+	if (!sub || sub === "-h" || sub === "--help") {
+		printHelp();
+		return;
+	}
+	if (sub !== "generate") {
+		console.error(RED(`Unknown subcommand: ${sub}`));
+		printHelp();
+		process.exit(2);
+	}
+	const flags = parseFlags(argv.slice(2));
+	try {
+		await runGenerate({ flags });
+	} catch (err: any) {
+		console.error(RED(`\nError: ${err?.message ?? String(err)}`));
+		process.exit(1);
+	}
+}

test/workflow-generator.test.ts
@@ -1,0 +1,222 @@
+// Unit tests for src/utils/workflow-generator.ts and the retry loop in
+// src/commands/workflow.ts. The LLM is fully mocked — no network calls.
+
+import test from "node:test";
+import assert from "node:assert/strict";
+import { mkdtemp, readFile, rm } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+
+import {
+	buildMessages,
+	buildSystemPrompt,
+	stripCodeFences,
+	generateWorkflow,
+	type LlmClient,
+	type LlmMessage,
+} from "../src/utils/workflow-generator.ts";
+import { runGenerate } from "../src/commands/workflow.ts";
+import type { SkillMetadata } from "../src/skills.ts";
+
+const SKILLS: SkillMetadata[] = [
+	{ name: "gmail", description: "Read and send email", directory: "/x/skills/gmail", filePath: "/x/skills/gmail/SKILL.md" },
+	{ name: "slack", description: "Post to Slack", directory: "/x/skills/slack", filePath: "/x/skills/slack/SKILL.md" },
+	{ name: "summarize", description: "Summarize text", directory: "/x/skills/summarize", filePath: "/x/skills/summarize/SKILL.md" },
+];
+
+const VALID_YAML = `name: morning-digest
+description: Summarize unread emails and post to Slack each morning.
+steps:
+  - skill: gmail
+    prompt: Fetch unread emails.
+  - skill: summarize
+    prompt: Compose a digest.
+  - skill: slack
+    prompt: Post the digest.
+    channel: "#daily-digest"
+`;
+
+const INVALID_YAML_MISSING_NAME = `description: no name here
+steps:
+  - skill: gmail
+    prompt: hi
+`;
+
+// ── buildSystemPrompt / buildMessages ──────────────────────────────────
+
+test("buildSystemPrompt embeds the schema text and the skill list", () => {
+	const sys = buildSystemPrompt(SKILLS);
+	assert.ok(sys.includes("<schema>"), "system prompt missing <schema> tag");
+	assert.ok(sys.includes('"$id": "https://gitagent.dev/spec/workflow.schema.json"'), "system prompt missing schema $id");
+	assert.ok(sys.includes("- gmail: Read and send email"), "system prompt missing gmail skill");
+	assert.ok(sys.includes("- slack: Post to Slack"), "system prompt missing slack skill");
+	assert.ok(sys.includes("Output ONLY valid YAML"), "system prompt missing rule about raw YAML");
+});
+
+test("buildSystemPrompt handles empty skill list with a fallback hint", () => {
+	const sys = buildSystemPrompt([]);
+	assert.ok(sys.includes("no installed skills detected"), "system prompt missing empty-skills fallback");
+});
+
+test("buildMessages includes two few-shot pairs and the user prompt", () => {
+	const messages = buildMessages({ prompt: "Do the thing", skills: SKILLS });
+	assert.equal(messages[0].role, "system");
+	assert.equal(messages[1].role, "user");
+	assert.equal(messages[2].role, "assistant");
+	assert.equal(messages[3].role, "user");
+	assert.equal(messages[4].role, "assistant");
+	assert.equal(messages[5].role, "user");
+	assert.equal(messages[5].content, "Do the thing");
+});
+
+test("buildMessages wraps refine-mode prompts with the previous YAML and instruction", () => {
+	const messages = buildMessages({
+		prompt: "Add an approval step before the Slack post.",
+		skills: SKILLS,
+		previousWorkflow: VALID_YAML,
+	});
+	const last = messages[messages.length - 1];
+	assert.equal(last.role, "user");
+	assert.ok(last.content.includes("Here is the current workflow"));
+	assert.ok(last.content.includes("Add an approval step before the Slack post."));
+	assert.ok(last.content.includes("morning-digest"));
+	assert.ok(last.content.includes("Return the complete updated workflow as YAML — not a diff."));
+});
+
+// ── stripCodeFences ────────────────────────────────────────────────────
+
+test("stripCodeFences removes generic fenced code", () => {
+	const input = "```\nname: foo\n```\n";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+test("stripCodeFences removes yaml-tagged fences", () => {
+	const input = "```yaml\nname: foo\n```";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+test("stripCodeFences leaves unfenced YAML alone", () => {
+	const input = "name: foo\n";
+	assert.equal(stripCodeFences(input), "name: foo");
+});
+
+// ── generateWorkflow with an injected LLM ──────────────────────────────
+
+test("generateWorkflow returns the LLM output after stripping fences", async () => {
+	const captured: { messages: LlmMessage[] } = { messages: [] };
+	const llm: LlmClient = async (messages) => {
+		captured.messages = messages;
+		return "```yaml\n" + VALID_YAML + "```";
+	};
+	const out = await generateWorkflow({
+		prompt: "summarize emails and post to slack",
+		skills: SKILLS,
+		llm,
+	});
+	assert.equal(out.trim().startsWith("name: morning-digest"), true);
+	assert.equal(captured.messages.length, 6);
+	assert.equal(captured.messages[0].role, "system");
+});
+
+test("generateWorkflow throws if prompt is empty", async () => {
+	await assert.rejects(
+		() => generateWorkflow({ prompt: "   ", skills: SKILLS, llm: async () => VALID_YAML }),
+		/prompt is required/,
+	);
+});
+
+// ── Retry loop in runGenerate ──────────────────────────────────────────
+
+test("runGenerate retries when validation fails, then writes the file when the second attempt is valid", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitagent-test-"));
+	try {
+		let calls = 0;
+		const llm: LlmClient = async (messages) => {
+			calls++;
+			const userMsg = messages[messages.length - 1].content;
+			if (calls === 1) return INVALID_YAML_MISSING_NAME;
+			// Second attempt: ensure the retry prompt included the validation error.
+			assert.ok(userMsg.includes("schema validation"), `retry user message did not mention validation: ${userMsg}`);
+			return VALID_YAML;
+		};
+		const result = await runGenerate({
+			flags: {
+				dir,
+				prompt: "summarize unread emails and post to Slack",
+				dryRun: false,
+			},
+			llm,
+		});
+		assert.equal(calls, 2);
+		assert.ok(result.filePath, "expected a written file path");
+		assert.equal(result.filePath!.endsWith("workflows/morning-digest.yaml"), true);
+		const written = await readFile(result.filePath!, "utf-8");
+		assert.ok(written.includes("name: morning-digest"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate honours --dry-run by returning YAML without writing", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitagent-test-"));
+	try {
+		const llm: LlmClient = async () => VALID_YAML;
+		const result = await runGenerate({
+			flags: { dir, prompt: "x", dryRun: true },
+			llm,
+		});
+		assert.equal(result.filePath, undefined);
+		assert.ok(result.yaml.includes("name: morning-digest"));
+		// workflows/ must not have been created.
+		await assert.rejects(() => readFile(join(dir, "workflows", "morning-digest.yaml"), "utf-8"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate gives up after MAX_RETRIES and throws", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitagent-test-"));
+	try {
+		let calls = 0;
+		const llm: LlmClient = async () => {
+			calls++;
+			return INVALID_YAML_MISSING_NAME;
+		};
+		await assert.rejects(
+			() => runGenerate({ flags: { dir, prompt: "x", dryRun: true }, llm }),
+			/Validation failed after retries/,
+		);
+		assert.equal(calls, 3); // 1 initial + 2 retries
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});
+
+test("runGenerate refine mode reads previous YAML and passes it to the LLM", async () => {
+	const dir = await mkdtemp(join(tmpdir(), "gitagent-test-"));
+	try {
+		const { writeFile, mkdir } = await import("node:fs/promises");
+		await mkdir(join(dir, "workflows"), { recursive: true });
+		const refinePath = join(dir, "workflows", "starter.yaml");
+		await writeFile(refinePath, VALID_YAML, "utf-8");
+
+		let observed = "";
+		const llm: LlmClient = async (messages) => {
+			observed = messages[messages.length - 1].content;
+			return VALID_YAML;
+		};
+		await runGenerate({
+			flags: {
+				dir,
+				prompt: "add an approval step before slack",
+				refine: "workflows/starter.yaml",
+				dryRun: true,
+			},
+			llm,
+		});
+		assert.ok(observed.includes("Here is the current workflow"));
+		assert.ok(observed.includes("add an approval step before slack"));
+	} finally {
+		await rm(dir, { recursive: true, force: true });
+	}
+});